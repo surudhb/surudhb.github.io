0ef5201340e97e44b2c425a3d14b62a081257a98
Surudh Bhutani <bsurudh@gmail.com>
2020-05-10 21:59:41 -0400

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Navbar from "react-bootstrap/Navbar"
+
+export const Navbar = () => {
+  return <div></div>
+}
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import "../styles/global.scss"
+import { Container } from "react-bootstrap"
+
+export default () => <Container>Hello World!</Container>
