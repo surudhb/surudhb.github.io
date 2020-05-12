5927871321067f2d2e5318948f42af059d78dfd7
Surudh Bhutani <bsurudh@gmail.com>
2020-05-12 18:04:21 -0400

    added aurebesh

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -13,7 +13,7 @@ export default ({ data }) => {
         <Container fluid className="text-left">
           <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
           {data.allMarkdownRemark.edges.map(({ node }) => (
-            <div key={node.id}>
+            <div key={node.id} className="aurebesh">
               <Link to={node.fields.slug}>
                 <h3>
                   {node.frontmatter.title} - {node.frontmatter.date}
