b9be8e029b765487b950ed4d0a51f545fc3b66d4
Surudh Bhutani <bsurudh@gmail.com>
2020-05-19 20:20:37 -0400

    init commit for theming

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,37 @@
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
+import { faBorderNone } from "@fortawesome/free-solid-svg-icons"
 
-export default () => (
-  <footer className={styles.sticky}>
-    <Container fluid className="text-center">
-      <Row>
-        <Col className="text-muted">
-          Surudh Bhutani &copy; {new Date().getFullYear()}. Made with{" "}
-          <span style={{ color: "cyan" }}>❤</span> &{" "}
-          <a href="https://www.gatsbyjs.org/">Gatsby</a>
-        </Col>
-      </Row>
-    </Container>
-  </footer>
-)
+export default () => {
+  const handleMouseOver = e => {
+    console.log(e.target)
+  }
+  return (
+    <footer className={styles.sticky}>
+      <Container fluid className="text-center">
+        <Row>
+          <Col className="text-muted">
+            Surudh Bhutani &copy; {new Date().getFullYear()}. Made with{" "}
+            <button style={styles.buttonStyle} onMouseOver={handleMouseOver}>
+              ❤
+            </button>{" "}
+            & <a href="https://www.gatsbyjs.org/">Gatsby</a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
 
 const styles = {
   sticky: {
     left: 0,
     bottom: 0,
     width: "100%",
+  },
+  buttonStyle: {
+    color: "cyan",
+    border: "none",
+    background: "white",
   },
 }
