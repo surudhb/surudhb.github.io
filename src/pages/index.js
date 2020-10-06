5b2472a8779057905bb9678af66bb70b7227dba2
Surudh Bhutani <bsurudh@gmail.com>
2020-10-06 12:27:14 -0400

    changed dialogues for typewriter

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,26 +8,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Typewriter from "typewriter-effect"
 
 export default ({ data }) => {
-  const { unemployed, firstName, lastName, dialogues } = data.site.siteMetadata
+  const { firstName, lastName, dialogues } = data.site.siteMetadata
   const { toString } = useContext(ThemeContext)
   const typewriter_dialogues = dialogues[`${toString()}`]
-  unemployed &&
-    typewriter_dialogues.unshift("Hey! I am looking for new opportunities.")
   return (
     <PageLayout>
       <SEO title="Home" />
       <div className="text-center px-0 mx-0" style={{ marginTop: "15%" }}>
-        {unemployed && (
-          <Typewriter
-            options={{
-              strings: typewriter_dialogues,
-              delay: 20,
-              deleteSpeed: 30,
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        )}
+        <Typewriter
+          options={{
+            strings: typewriter_dialogues,
+            delay: 20,
+            deleteSpeed: 30,
+            autoStart: true,
+            loop: true,
+          }}
+        />
         <h1 className="display-1" style={{ fontSize: "7vmax" }}>
           <span className="first-name">{firstName}</span>&nbsp;
           <span className="last-name">{lastName}</span>
