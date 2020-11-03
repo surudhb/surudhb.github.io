3507b7c92f10315f90891e43504fb5d019142bf0
surudhb <45405267+surudhb@users.noreply.github.com>
2020-11-02 21:06:58 -0500

    Light shadows (#2)
    * added box shadows to icons, blog links and project links
    
    * added carousel for project images, removed extraneous containers

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -4,11 +4,11 @@ import Header from "./Header"
 import Footer from "./Footer"
 
 export default ({ children }) => (
-  <Container fluid className="px-0 theme-light app-container">
+  <Container fluid className="px-0 app-container theme-light">
     <Header />
-    <Container fluid className="pt-5 mt-5 text-center min-vh-100">
+    <div fluid className="pt-5 mt-5 text-center min-vh-100">
       {children}
-    </Container>
+    </div>
     <Footer />
   </Container>
 )
